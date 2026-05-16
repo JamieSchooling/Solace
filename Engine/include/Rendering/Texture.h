ce72eb37033393d0dce54df1683b37768219a267
JamieSchooling <99397650+JamieSchooling@users.noreply.github.com>
2026-05-16 21:00:41 +0100

    feat: Add image texture loading

--- Engine/include/Rendering/Texture.h
+++ Engine/include/Rendering/Texture.h
@@ -3,18 +3,22 @@
 #include <glm/glm.hpp>
 #include <cstdint>
 
+#include <filesystem>
+
 class Texture
 {
 public:
 	Texture(glm::ivec2 size);
+	Texture(std::filesystem::path imagePath);
 	~Texture();
 
 	inline uint32_t GetID() const { return m_id; }
 
 	void Recreate(glm::ivec2 size);
+	void Recreate(std::filesystem::path imagePath);
 private:
 	uint32_t m_id;
 
-	void Init(glm::ivec2 size);
+	void Init(glm::ivec2 size, unsigned char* data = nullptr);
 	void Delete();
 };
--- Engine/src/Rendering/Texture.cpp
+++ Engine/src/Rendering/Texture.cpp
@@ -2,9 +2,27 @@
 
 #include <glad/gl.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
+#include <iostream>
+
 Texture::Texture(glm::ivec2 size)
 {
 	Init(size);
+}
+
+Texture::Texture(std::filesystem::path imagePath)
+{
+	int width, height, numChannels;
+	unsigned char* data = stbi_load(imagePath.string().c_str(), &width, &height, &numChannels, 0);
+	if (data)
+	{
+		std::cout << "Image: " << imagePath << " loaded." << std::endl;
+		std::cout << "Width: " << width << "Height: " << height << std::endl;
+	}
+	Init({ width, height }, data); 
+	stbi_image_free(data);
 }
 
 Texture::~Texture()
@@ -18,7 +36,17 @@ void Texture::Recreate(glm::ivec2 size)
 	Init(size);
 }
 
-void Texture::Init(glm::ivec2 size)
+void Texture::Recreate(std::filesystem::path imagePath)
+{
+	Delete();
+	int width, height, numChannels;
+	unsigned char* data = stbi_load(imagePath.string().c_str(), &width, &height, &numChannels, 4);
+	Init({ width, height }, data);
+	stbi_image_free(data);
+}
+
+
+void Texture::Init(glm::ivec2 size, unsigned char* data)
 {
 	glCreateTextures(GL_TEXTURE_2D, 1, &m_id);
 
@@ -29,6 +57,12 @@ void Texture::Init(glm::ivec2 size)
 	glTextureParameteri(m_id, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	glTextureStorage2D(m_id, 1, GL_RGBA8, size.x, size.y);
+
+	if (data)
+	{
+		glTextureSubImage2D(m_id, 0, 0, 0, size.x, size.y, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateTextureMipmap(m_id);
+	}
 }
 
 void Texture::Delete()

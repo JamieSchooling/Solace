ff0814a99707b5b9e5e48ab6467e92065be5506a
JamieSchooling <99397650+JamieSchooling@users.noreply.github.com>
2026-05-26 03:04:25 +0100

    feat: Implement default editor cam transform

--- Editor/src/Core/EditorSystem.cpp
+++ Editor/src/Core/EditorSystem.cpp
@@ -56,6 +56,8 @@ void EditorSystem::Start(const SubsystemParams& params)
 	OpenWindow<AssetBrowser>();
 
 	InputSystem::Get().AddFourComponentAction("Move", InputBinding::W, InputBinding::A, InputBinding::S, InputBinding::D);
+	m_editorCamTransform.Position = glm::vec3(4.0f, 4.0f, 4.0f);
+	m_editorCamTransform.Rotation = glm::quat(glm::radians(glm::vec3(-45.0f, 45.0f, 0.0f)));
 }
 
 void EditorSystem::Shutdown()

a7512456b1be41b7c82356e7454c98daf5a6584e
JamieSchooling <99397650+JamieSchooling@users.noreply.github.com>
2026-06-12 23:42:13 +0100

    feat: Implement drag to reorder scene hierarchy

--- Editor/include/Windows/SceneHierarchy.h
+++ Editor/include/Windows/SceneHierarchy.h
@@ -3,6 +3,8 @@
 #include "Windows/EditorWindow.h"
 #include "Menu/MenuRegistry.h"
 
+#include <imgui.h>
+
 class SceneHierarchy : public EditorWindow
 {
 public:
@@ -12,4 +14,8 @@ public:
 	MENU_ITEM("Window/General/Hierarchy", 410, Open);
 protected:
 	void DrawContent(entt::entity& selected, Scene& scene) override;
+
+private:
+	void DrawEntity(Scene& scene, entt::entity entity, entt::entity& selected, ImGuiTreeNodeFlags flags);
+	void DrawDropSlot(Scene& scene, size_t insertIndex);
 };
--- Editor/src/Windows/SceneHierarchy.cpp
+++ Editor/src/Windows/SceneHierarchy.cpp
@@ -1,6 +1,5 @@
 #include "Windows/SceneHierarchy.h"
 
-#include <imgui.h>
 #include <Scenes/NameComponent.h>
 #include <Transform/Transform.h>
 
@@ -20,6 +19,7 @@ void SceneHierarchy::DrawContent(entt::entity& selected, Scene& scene)
 		{
 			entt::entity entity = scene.Registry.create();
 			scene.Registry.emplace<NameComponent>(entity).Name = "Entity";
+			scene.Registry.emplace<OrderComponent>(entity).Order = scene.Registry.view<entt::entity>().size();
 			scene.Registry.emplace<Transform>(entity);
 			selected = entity;
 		}
@@ -39,36 +39,14 @@ void SceneHierarchy::DrawContent(entt::entity& selected, Scene& scene)
 			return scene.Registry.get<OrderComponent>(a).Order < scene.Registry.get<OrderComponent>(b).Order;
 		});
 
+		size_t pos = 0;
 		for (auto entity : entities)
 		{
-			std::string m_name = "Unnamed Entity";
-			if (scene.Registry.all_of<NameComponent>(entity))
-			{
-				m_name = scene.Registry.get<NameComponent>(entity).Name;
-			}
-			ImGui::PushID((int)entity);
-			ImGuiTreeNodeFlags nodeFlags = baseNodeFlags;
-			if (entity == selected)
-				nodeFlags |= ImGuiTreeNodeFlags_Selected;
-			if (ImGui::TreeNodeEx(m_name.c_str(), nodeFlags))
-			{
-				if (ImGui::IsItemClicked() && !ImGui::IsItemToggledOpen())
-				{
-					selected = entity;
-				}
-				ImGui::TreePop();
-			}
-			if (ImGui::BeginPopupContextItem())
-			{
-				if (ImGui::MenuItem("Delete"))
-				{
-					if (selected == entity) { selected = entt::null; }
-					scene.Registry.destroy(entity);
-				}
-				ImGui::EndPopup();
-			}
-			ImGui::PopID();
+			DrawDropSlot(scene, pos);
+			DrawEntity(scene, entity, selected, baseNodeFlags);
+			pos++;
 		}
+		DrawDropSlot(scene, pos);
 		ImGui::TreePop();
 	}
 
@@ -77,4 +55,83 @@ void SceneHierarchy::DrawContent(entt::entity& selected, Scene& scene)
 	{
 		selected = entt::null;
 	}
+}
+
+void SceneHierarchy::DrawEntity(Scene& scene, entt::entity entity, entt::entity& selected, ImGuiTreeNodeFlags flags)
+{
+	std::string name = "Unnamed Entity";
+	if (scene.Registry.all_of<NameComponent>(entity))
+	{
+		name = scene.Registry.get<NameComponent>(entity).Name;
+	}
+	ImGui::PushID((int)entity);
+	ImGuiTreeNodeFlags nodeFlags = flags;
+	if (entity == selected)
+		nodeFlags |= ImGuiTreeNodeFlags_Selected;
+	if (ImGui::TreeNodeEx(name.c_str(), nodeFlags))
+	{
+		if (ImGui::IsItemClicked() && !ImGui::IsItemToggledOpen())
+		{
+			selected = entity;
+		}
+		ImGui::TreePop();
+	}
+	if (ImGui::BeginDragDropSource())
+	{
+		ImGui::SetDragDropPayload("SCENE_ENTITY", &entity, sizeof(entt::entity), ImGuiCond_Once);
+		ImGui::EndDragDropSource();
+	}
+	if (ImGui::BeginPopupContextItem())
+	{
+		if (ImGui::MenuItem("Delete"))
+		{
+			if (selected == entity) { selected = entt::null; }
+			scene.Registry.destroy(entity);
+		}
+		ImGui::EndPopup();
+	}
+	ImGui::PopID();
+}
+
+void SceneHierarchy::DrawDropSlot(Scene& scene, size_t insertIndex)
+{
+	ImGui::PushID((int)insertIndex);
+
+	ImGui::InvisibleButton("DropSlot", ImVec2(-1.0f, 2.0f));
+
+	if (ImGui::BeginDragDropTarget())
+	{
+		if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("SCENE_ENTITY"))
+		{
+			entt::entity dragged = *static_cast<entt::entity*>(payload->Data);
+
+			auto view = scene.Registry.view<entt::entity>();
+
+			std::vector<entt::entity> entities(view.begin(), view.end());
+
+			std::sort(entities.begin(), entities.end(), [&](entt::entity a, entt::entity b)
+			{
+				return scene.Registry.get<OrderComponent>(a).Order < scene.Registry.get<OrderComponent>(b).Order;
+			});
+
+			auto it = std::find(entities.begin(), entities.end(), dragged);
+			size_t oldIndex = scene.Registry.get<OrderComponent>(dragged).Order;
+
+			entities.erase(it);
+
+			if (insertIndex > oldIndex)
+				insertIndex--;
+
+			entities.insert(entities.begin() + insertIndex, dragged);
+
+			for (size_t i = 0; i < entities.size(); ++i)
+			{
+				scene.Registry.get<OrderComponent>(entities[i]).Order = i;
+			}
+		}
+
+		ImGui::EndDragDropTarget();
+	}
+
+	ImGui::PopID();
 }
